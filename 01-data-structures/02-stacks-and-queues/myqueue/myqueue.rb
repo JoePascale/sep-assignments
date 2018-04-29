78b9471706d2997d846ef4ac431839b64a61c72d
JoePascale <joe.m.pascale@gmail.com>
2018-04-29 12:42:58 -0700

    finished myqueue & txt writing assignment

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -8,11 +8,17 @@ class MyQueue
   end
 
   def enqueue(element)
+    @tail = element
+    @queue.empty? ? @head = element : @head = @queue[0]
+    @queue[0] = element
   end
 
   def dequeue
+    @head = @queue[1]
+    @queue = @queue[1...-1]
   end
 
   def empty?
+    @queue.any? ? false : true
   end
 end
