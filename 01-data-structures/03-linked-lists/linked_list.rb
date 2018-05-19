f6f8d388c8054ab376b9b2c4b6fad139796c3275
JoePascale <joe.m.pascale@gmail.com>
2018-05-19 12:49:33 -0700

    completed linked list project & first assignment answer

--- 01-data-structures/03-linked-lists/linked_list.rb
+++ 01-data-structures/03-linked-lists/linked_list.rb
@@ -35,53 +35,29 @@ class LinkedList
 
   # This method prints out a representation of the list.
   def print
-=begin
-    list = []
     current = @head
-    while current.next != nil
-      list << current
-      current = current.next
-    end
-    list << current
-    list.each do |node|
-      p node.data
-    end
-=end
-    current = @head
-    while current != nil
-      p current.data
-      current = current.next
+    puts current.data
+
+    while (current = current.next)
+      puts current.data
     end
   end
 
 
   # This method removes `node` from the list and must keep the rest of the list intact.
   def delete(node)
-    #keep track of 2 temp variables: previous & current
     current = @head
-    #next_node = @head.next
-    previous = nil
-
-    #if @head is node to be deleted, set new head
-    #if @head == node
-      #@head = current.next
-    #else
-=begin
-      while (current.next != nil) && (current.next.node != node)
-        current = current.next
-      end
-      unless current.next == nil
-        current.next = current.next.next
-      end
-=end
-    #end
-    #make sure previous node's next points to (node)
-    #previous.next = current.next
+    if current == node
+      @head = current.next
+    elsif current.next == node
+      @head.next = current.next.next
+    elsif @tail == node
+      @tail = current.next
+    end
   end
 
   # This method adds `node` to the front of the list and must set the list's head to `node`.
   def add_to_front(node)
-    #add node to front of list
     if @head != nil
       current = @head
       @head = node
@@ -93,9 +69,6 @@ class LinkedList
 
   # This method removes and returns the first node in the Linked List and must set Linked List's head to the second node.
   def remove_front
-    #need temp variable here to store for multiple add_to_front calls
-    #new_front = @head
     @head = @head.next
-    #front
   end
 end
