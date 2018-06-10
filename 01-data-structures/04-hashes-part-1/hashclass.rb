fad58a2f093228a0f9de7eb96123127c18dd8dec
JoePascale <joe.m.pascale@gmail.com>
2018-06-10 12:17:32 -0700

    updated Hash Class methods

--- 01-data-structures/04-hashes-part-1/hashclass.rb
+++ 01-data-structures/04-hashes-part-1/hashclass.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class HashClass
 
   def initialize(size)
@@ -8,41 +10,48 @@ class HashClass
   #use key to figure out where to put @items
   #assign to that address the value where that gets put in
   def []=(key, value)
-    @items[index(key,@items.size)] = value
+    address = index(key,@items.size)
+    p value
+    p address
+    p @items[address]
+    if @items[address] == nil
+      @items[address] = HashItem.new(key,value)
+      #p @items[address]
+    elsif @items[address] == value
+      #p @items[address]
+    elsif @items[address] != value
+      resize()
+      address = index(key,@items.size)
+      @items[address] = HashItem.new(key,value)
+      #p @items[address]
+    end
   end
 
   #lookup method (accessor)
   #accessor - any function that returns a particular member of class (externally)
   def [](key)
-    @items[index(key,@items.size)]
+    @items[index(key,@items.size)].value
   end
 
+  #make temporary copy of array (@items), then move from old array to new
+  #double size array using insertion method
   def resize
     #create new array & fill it with data from previous
-    copy = @items
+    copy_array = @items
+    @items = Array.new(@items.size*2)
     #call insertion function, then insert function should call hashing function
-    #[]=(nil, nil)
-=begin
-    @items.each do |e|
-      copy << e
+    copy_array.each do |e|
+      unless e.nil?
+        self[e.key] = e.value
+      end
     end
-=end
-    #make temporary copy of array (@items), then move from old array to new
-    #double sized array using insertion method
-    @items = @items[(@items.length)*2]
-    puts @items
   end
 
   # Returns a unique, deterministically reproducible index into an array
   # We are hashing based on strings, let's use the ascii value of each string as
   # a starting point.
-  #shouldn't modify anything about the class
   def index(key, size)
-    total = 0
-    key.each_byte do |c|
-      total += c
-    end
-    total % size
+    key.sum % size
   end
 
   # Simple method to return the number of items in the hash
