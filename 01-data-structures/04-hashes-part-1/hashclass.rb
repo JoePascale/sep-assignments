6f77fe188f5598d2cd3e0b3bcc75debb6dca29dc
JoePascale <joe.m.pascale@gmail.com>
2018-06-10 13:10:32 -0700

    completed Hash Class & confirmed rspec tests pass

--- 01-data-structures/04-hashes-part-1/hashclass.rb
+++ 01-data-structures/04-hashes-part-1/hashclass.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 class HashClass
 
   def initialize(size)
@@ -7,39 +5,28 @@ class HashClass
   end
 
   #insertion method (mutator)
-  #use key to figure out where to put @items
-  #assign to that address the value where that gets put in
   def []=(key, value)
     address = index(key,@items.size)
-    p value
-    p address
-    p @items[address]
     if @items[address] == nil
       @items[address] = HashItem.new(key,value)
-      #p @items[address]
-    elsif @items[address] == value
-      #p @items[address]
-    elsif @items[address] != value
-      resize()
+    elsif @items[address].key != key
+      self.resize
+      self[key] = value
+    elsif @items[address].value != value
+      self.resize
       address = index(key,@items.size)
       @items[address] = HashItem.new(key,value)
-      #p @items[address]
     end
   end
 
   #lookup method (accessor)
-  #accessor - any function that returns a particular member of class (externally)
   def [](key)
     @items[index(key,@items.size)].value
   end
 
-  #make temporary copy of array (@items), then move from old array to new
-  #double size array using insertion method
   def resize
-    #create new array & fill it with data from previous
     copy_array = @items
     @items = Array.new(@items.size*2)
-    #call insertion function, then insert function should call hashing function
     copy_array.each do |e|
       unless e.nil?
         self[e.key] = e.value
@@ -48,13 +35,12 @@ class HashClass
   end
 
   # Returns a unique, deterministically reproducible index into an array
-  # We are hashing based on strings, let's use the ascii value of each string as
-  # a starting point.
+  # hashing based on strings, ascii value of each string is starting point
   def index(key, size)
     key.sum % size
   end
 
-  # Simple method to return the number of items in the hash
+  # method to return the number of items in the hash
   def size
     @items.length
   end
