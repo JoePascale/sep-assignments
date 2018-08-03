aab59c631ed2e80ce92f6618827c11bd4ef0664d
JoePascale <joe.m.pascale@gmail.com>
2018-08-02 20:10:19 -0700

    completed insert method & added > override to node class

--- 01-data-structures/06-trees/binary_tree/binary_search_tree.rb
+++ 01-data-structures/06-trees/binary_tree/binary_search_tree.rb
@@ -7,11 +7,32 @@ class BinarySearchTree
   end
 
   def insert(root, node)
-    
+    if root
+      if root > node
+        if root.left == nil
+          root.left = node
+        else
+          insert(root.left, node)
+        end
+      else
+        if root.right == nil
+          root.right = node
+        else
+          insert(root.right, node)
+        end
+      end
+    end
+    #use recursion, not while loops
+    #compare roots value with node value,
+    #first param is what subtree I'm concerned with
+    #if node is less than, insert in left tree
+    #when no left or right child, insert node as value
   end
 
   # Recursive Depth First Search
   def find(root, data)
+    #similar to insert method
+    #keep looking down the tree to compare nodes
   end
 
   def delete(root, data)
@@ -19,5 +40,8 @@ class BinarySearchTree
 
   # Recursive Breadth First Search
   def printf(children=nil)
+    #could possibly create an array that keeps
+    #elements in queue & removes ones that have
+    #already been printed
   end
 end
--- 01-data-structures/06-trees/binary_tree/node.rb
+++ 01-data-structures/06-trees/binary_tree/node.rb
@@ -5,5 +5,21 @@ class Node
   attr_accessor :right
 
   def initialize(title, rating)
+    @title = title
+    @rating = rating
   end
+
+  def >(node)
+    #compare self instance & other instance
+    if self.rating > node.rating
+      return true
+    end
+  end
+=begin
+  def <(node)
+    if self.rating < node.rating
+      return true
+    end
+  end
+=end
 end
