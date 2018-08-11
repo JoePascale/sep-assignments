f9559c04ec0f9162adae7b87e53188756a83362a
JoePascale <joe.m.pascale@gmail.com>
2018-08-11 15:41:20 -0700

    completed find & delete methods

--- 01-data-structures/06-trees/binary_tree/binary_search_tree.rb
+++ 01-data-structures/06-trees/binary_tree/binary_search_tree.rb
@@ -33,15 +33,57 @@ class BinarySearchTree
   def find(root, data)
     #similar to insert method
     #keep looking down the tree to compare nodes
+    if root
+      if root.title != data
+        if root.left
+          find(root.left, data)
+        else
+          find(root.right, data)
+        end
+      else
+        root
+      end
+    end
   end
 
   def delete(root, data)
+    if root && data
+      if root.title != data
+        if root.left
+          if root.left.title != data
+            delete(root.left, data)
+          else
+            root.left = nil
+          end
+        else #root.right
+          if root.right.title != data
+            delete(root.right, data)
+          else
+            root.right = nil
+          end
+        end
+      end
+    end
   end
 
   # Recursive Breadth First Search
-  def printf(children=nil)
+  def printf
     #could possibly create an array that keeps
     #elements in queue & removes ones that have
     #already been printed
+    queue = [@root, @root.left, @root.right]
+    #loop: print the first thing in the array, with what's
+    #left, grab it's children, add them to queue
+    while queue.size != 0
+      p "#{queue[0].title}: #{queue[0].rating}"
+      queue.shift
+      next_thing = queue[0]
+      #queue.push(next_thing.left) if next_thing.left
+      #queue.push(next_thing.right) if next_thing.right
+
+      #remove these lines below likely
+      #puts queue[1].title
+      #puts queue[2].title
+    end
   end
 end
