ae700e4db915657777b2accba2f7eb04e90bcfc1
JoePascale <joe.m.pascale@gmail.com>
2018-08-16 21:47:09 -0700

    completed print method

--- 01-data-structures/06-trees/binary_tree/binary_search_tree.rb
+++ 01-data-structures/06-trees/binary_tree/binary_search_tree.rb
@@ -22,17 +22,10 @@ class BinarySearchTree
         end
       end
     end
-    #use recursion, not while loops
-    #compare roots value with node value,
-    #first param is what subtree I'm concerned with
-    #if node is less than, insert in left tree
-    #when no left or right child, insert node as value
   end
 
   # Recursive Depth First Search
   def find(root, data)
-    #similar to insert method
-    #keep looking down the tree to compare nodes
     if root
       if root.title != data
         if root.left
@@ -68,22 +61,17 @@ class BinarySearchTree
 
   # Recursive Breadth First Search
   def printf
-    #could possibly create an array that keeps
-    #elements in queue & removes ones that have
-    #already been printed
     queue = [@root, @root.left, @root.right]
     #loop: print the first thing in the array, with what's
     #left, grab it's children, add them to queue
     while queue.size != 0
-      p "#{queue[0].title}: #{queue[0].rating}"
+      puts "#{queue[0].title}: #{queue[0].rating}\n"
       queue.shift
       next_thing = queue[0]
-      #queue.push(next_thing.left) if next_thing.left
-      #queue.push(next_thing.right) if next_thing.right
-
-      #remove these lines below likely
-      #puts queue[1].title
-      #puts queue[2].title
+      if next_thing != nil
+        queue.push(next_thing.left) if next_thing.left
+        queue.push(next_thing.right) if next_thing.right
+      end
     end
   end
 end
